03f92596658a826e72c1ab6b934405dfe328df31
root <root@localhost.localdomain>
2021-09-22 20:16:41 +0200

    date fix.

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -24,11 +24,6 @@ var store = [{
         "tags": ["wordpress","shell","enumeration"],
         "url": "http://localhost:4000/smb_relay.md/",
         "teaser":"http://localhost:4000/assets/images/wpsUsersScan/logo.png"},{
-        "title": "simpleCurlScan",
-        "excerpt":"¿Qué es simpleCurlScan? simpleCurlScan es una herramienta creada en bash. Creé este pequeño script en base a la necesidad de realizar escaneo de directorios en una máquina remota la cual estaba bastante “desnuda”, en el sentido que python no estaba instalado (por ejemplo) o varios programas de linux que són...","categories": ["script"],
-        "tags": ["shell","enumeration","curl","web"],
-        "url": "http://localhost:4000/curl/",
-        "teaser":"http://localhost:4000/assets/images/curl/logo.png"},{
         "title": "Denial Of Service",
         "excerpt":"¿Qué es un ataque de Denegación de servicio? El ataque de Denegación de Servicio consiste en inundar de solicitudes un servicio o red para dejarlo inhabilitado contra los usuarios legítimos. En este artículo hablaremos de 2 tipos de ataques: DoS y DDoS. DoS El ataque DoS és un tipo de...","categories": ["dos"],
         "tags": ["Denial of Service"],
@@ -38,4 +33,9 @@ var store = [{
         "excerpt":"¿Qué es BloodHound? BloodHound es una herramienta enfocada en la enumeración de Active Directory. BloodHound ayuda a los atacantes a identificar rutas de ataque altamente complejas que de otra forma sería mas complicado visualizar. Esta herramienta es muy útil, ya que representa de forma gráfica los resultados, se utiliza tanto...","categories": ["Windows"],
         "tags": ["BloodHound","Domain Controller","Active Directory"],
         "url": "http://localhost:4000/bloodhound/",
-        "teaser":"http://localhost:4000/assets/images/bloodhound/logo.png"}]
+        "teaser":"http://localhost:4000/assets/images/bloodhound/logo.png"},{
+        "title": "simpleCurlScan",
+        "excerpt":"¿Qué es simpleCurlScan? simpleCurlScan es una herramienta creada en bash. Creé este pequeño script en base a la necesidad de realizar escaneo de directorios en una máquina remota la cual estaba bastante “desnuda”, en el sentido que python no estaba instalado (por ejemplo) o varios programas de linux que són...","categories": ["script"],
+        "tags": ["shell","enumeration","curl","web"],
+        "url": "http://localhost:4000/curl/",
+        "teaser":"http://localhost:4000/assets/images/curl/logo.png"}]
