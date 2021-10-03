758204faf1304bca8d900c3858dc1172f9c81f29
root <root@localhost.localdomain>
2021-10-03 09:39:09 +0200

    web update

--- _pages/about.md
+++ _pages/about.md
@@ -1,6 +1,17 @@
 ---
-title: "About"
-permalink: /about/
-date: 2019-02-15
+layout: single
+title: Mis Certificaciones
+date: 2021-08-31
+permalink: /certs/
+author_profile: true
 ---
+
+[eJPT (eLearnSecurity Junior Penetration Tester)](https://elearnsecurity.com/product/ejpt-certification/)
+
+
+![](/assets/images/certs/eJPT.png)
+
+[OSWPv3 (Offensive Security Wireless Professional)](https://www.offensive-security.com/wifu-oswp/) 
+
+![](/assets/images/certs/oswp.jpg)
 
--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -4,25 +4,10 @@ var store = [{
         "tags": ["shell","wireless","aircrack-ng","wireless_pentesting"],
         "url": "http://localhost:4000/airAttack/",
         "teaser":"http://localhost:4000/assets/images/airAttack/airAttack.jpg"},{
-        "title": "Mis Certificaciones",
-        "excerpt":"Mis certificaciones   eJPT (eLearnSecurity Junior Penetration Tester)      OSWPv3 (Offensive Security Wireless Professional)      ","categories": ["script"],
-        "tags": ["Offensive-Security","OSWP","eLearnSecurity","eJPT"],
-        "url": "http://localhost:4000/mis_certificaciones/",
-        "teaser":null},{
         "title": "wp_userScan",
         "excerpt":"¿Qué es wps_usersScan? wps_usersScan es un pequeño script en bash ideal para realizar la enumeración de usuarios en cualquier web que utilice Wordpress como CMS. Para utilizar la herramienta, es tan fácil como ejecutarla: ./wps_usersScan.sh Seguidamente, saldrá un pequeño input en el cual se tiene que introducir una dirección URL....","categories": ["script"],
         "tags": ["wordpress","shell","enumeration"],
         "url": "http://localhost:4000/wp_userScan/",
-        "teaser":"http://localhost:4000/assets/images/wpsUsersScan/logo.png"},{
-        "title": "wp_userScan",
-        "excerpt":"¿Qué es wps_usersScan? wps_usersScan es un pequeño script en bash ideal para realizar la enumeración de usuarios en cualquier web que utilice Wordpress como CMS. Para utilizar la herramienta, es tan fácil como ejecutarla: ./wps_usersScan.sh Seguidamente, saldrá un pequeño input en el cual se tiene que introducir una dirección URL....","categories": ["script"],
-        "tags": ["wordpress","shell","enumeration"],
-        "url": "http://localhost:4000/smb_relay/",
-        "teaser":"http://localhost:4000/assets/images/wpsUsersScan/logo.png"},{
-        "title": "wp_userScan",
-        "excerpt":"En  ","categories": ["script"],
-        "tags": ["wordpress","shell","enumeration"],
-        "url": "http://localhost:4000/smb_relay.md/",
         "teaser":"http://localhost:4000/assets/images/wpsUsersScan/logo.png"},{
         "title": "Denial Of Service",
         "excerpt":"¿Qué es un ataque de Denegación de servicio? El ataque de Denegación de Servicio consiste en inundar de solicitudes un servicio o red para dejarlo inhabilitado contra los usuarios legítimos. En este artículo hablaremos de 2 tipos de ataques: DoS y DDoS. DoS El ataque DoS és un tipo de...","categories": ["dos"],
